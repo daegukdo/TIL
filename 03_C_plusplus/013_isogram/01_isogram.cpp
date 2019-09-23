b8ffe37a2c7ed868bfca1ec6a3cb2b57ba8270cd
ddk <eornrsayclub@gmail.com>
2019-09-23 23:53:24 +0900

    c++ study codewar : issue description isogram

---
+++ 03_C_plusplus/013_isogram/01_isogram.cpp
@@ -0,0 +1,19 @@
+// c++ codewar
+/*
+issue description
+An isogram is a word that has no repeating letters, consecutive or non-consecutive. 
+Implement a function that determines whether a string that contains only letters is an isogram. 
+Assume the empty string is an isogram. 
+Ignore letter case.
+
+example : 
+isIsogram "Dermatoglyphics" == true
+isIsogram "moose" == false
+isIsogram "aba" == false
+*/
+
+int main()
+{
+
+	return 0;
+}
