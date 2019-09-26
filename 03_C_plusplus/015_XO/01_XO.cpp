b39d23788aa3a846f075012dad95adda5bd49aa4
ddk <eornrsayclub@gmail.com>
2019-09-26 21:45:29 +0900

    c++ study codewars : sum of two smallest numbers answer codewars : XO description

---
+++ 03_C_plusplus/015_XO/01_XO.cpp
@@ -0,0 +1,26 @@
+// c++ codewar
+/*
+issue description
+Check to see if a string has the same amount of 'x's and 'o's. 
+The method must return a boolean and be case insensitive. 
+The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false
+*/
+
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+
+	return 0;
+}
