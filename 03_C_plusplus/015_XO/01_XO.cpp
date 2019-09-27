9898eb737f33b2c493be206339bc5e600e0f2cf3
ddk <eornrsayclub@gmail.com>
2019-09-27 23:54:43 +0900

    c++ study codewars : add XO my answer

--- 03_C_plusplus/015_XO/01_XO.cpp
+++ 03_C_plusplus/015_XO/01_XO.cpp
@@ -12,15 +12,56 @@ XO("xooxx") => false
 XO("ooxXm") => true
 XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
 XO("zzoo") => false
+
+ref. URL : http://www.cplusplus.com/articles/DE18T05o/
 */
 
-#include <vector>
+#include <string>
+#include <iostream>
+#include <locale>
 
-using namespace std;
-
+bool XO(const std::string& str);
 
 int main()
 {
+	std::string a= "xXoxo";
+
+	bool r1 = XO(a);
 
 	return 0;
+}
+
+bool XO(const std::string& str)
+{
+	std::locale loc;
+
+	bool result = false;
+
+	char Xdeli = 'X';
+	char Oceli = 'O';
+
+	int Xcount = 0;
+	int Ocount = 0;
+
+	int count = str.length();
+
+	for(int i = 0; i<count; i++)
+	{
+		if(Xdeli == std::toupper(str[i], loc))
+		{
+			Xcount++;
+		}
+
+		if(Oceli == std::toupper(str[i], loc))
+		{
+			Ocount++;
+		}
+	}
+
+	if(Xcount == Ocount)
+	{
+		result = true;
+	}
+
+	return result;
 }
