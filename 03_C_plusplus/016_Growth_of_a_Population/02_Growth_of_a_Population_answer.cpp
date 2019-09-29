51657b2a12275ac77083dea6729579d4f5ed7f78
ddk <eornrsayclub@gmail.com>
2019-09-29 22:44:08 +0900

    c++ study codewar : add code & answer (growth of a population)

--- 03_C_plusplus/016_Growth_of_a_Population/02_Growth_of_a_Population_answer.cpp
+++ 03_C_plusplus/016_Growth_of_a_Population/02_Growth_of_a_Population_answer.cpp
@@ -33,27 +33,22 @@ Note: Don't forget to convert the percent parameter as a percentage in the body 
 class Arge
 {
 public:
-	static int nbYear(int p0, double percent, int aug, int p);
+    static int nbYear(int p0, double percent, int aug, int p)
+    {
+        //p0 = starting population
+        //percent = percent increase per year
+        //aug = new arrivals per year
+        //p = final population (when p0 reaches this number, end.)
+        int years(0);
+        while (p0 < p)
+        {
+            p0 += (p0 * (percent / 100)) + aug;
+            years++;
+        }
+        return years;
+    }
+    
 };
-
-int Arge::nbYear(int p0, double percent, int aug, int p)
-{
-	int year = 0;
-	int tempPopulation = 0;
-
-	if((p0 > 0) & (p >= p0))
-	{
-		tempPopulation = p0;
-
-		while (tempPopulation < p)
-		{
-			tempPopulation += (tempPopulation * (0.01 * percent)) + aug;
-			year++;
-		}
-	}
-
-	return year;
-}
 
 int main()
 {
