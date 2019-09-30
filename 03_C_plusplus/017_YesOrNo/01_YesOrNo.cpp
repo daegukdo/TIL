d47e731ec0799b2081fd3b87df39bf36b55dfc6f
ddk <eornrsayclub@gmail.com>
2019-09-30 22:19:18 +0900

    c++ study codewar : add code & answer (yes or no)

---
+++ 03_C_plusplus/017_YesOrNo/01_YesOrNo.cpp
@@ -0,0 +1,22 @@
+// c++ code-war
+/*
+issue description
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+*/
+#include<iostream>
+
+using namespace std;
+
+string bool_to_word(bool value)
+{
+	if(value)
+		return "Yes";
+	else
+		return "No";
+}
+
+int main()
+{
+
+	return 0;
+}
---
+++ 03_C_plusplus/017_YesOrNo/02_YesOrNo_answer.cpp
@@ -0,0 +1,19 @@
+// c++ code-war
+/*
+issue description
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+*/
+#include<iostream>
+
+using namespace std;
+
+string bool_to_word(bool value)
+{
+  return value ? "Yes" : "No";
+}
+
+int main()
+{
+
+	return 0;
+}
