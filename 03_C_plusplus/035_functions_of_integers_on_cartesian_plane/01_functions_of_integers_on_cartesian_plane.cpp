4dc16e3784576050e3c72af4f6be1ee3d4573b3d
DaeGuk Do <eornrsayclub@gmail.com>
2019-11-25 21:26:31 +0900

    c++ study codewar : add test code (Functions of Integers on Cartesian Plane)

--- 03_C_plusplus/035_functions_of_integers_on_cartesian_plane/01_functions_of_integers_on_cartesian_plane.cpp
+++ 03_C_plusplus/035_functions_of_integers_on_cartesian_plane/01_functions_of_integers_on_cartesian_plane.cpp
@@ -55,14 +55,56 @@ using namespace std;
 
 class Funij
 {
-
-  public:
-  static unsigned long long sumin(unsigned long long n);
-  static unsigned long long sumax(unsigned long long n);
-  static unsigned long long sumsum(unsigned long long n);
+public:
+	static unsigned long long sumin(unsigned long long n);
+	static unsigned long long sumax(unsigned long long n);
+	static unsigned long long sumsum(unsigned long long n);
 };
+
+unsigned long long Funij::sumin(unsigned long long n)
+{
+	unsigned long long sum = 0;
+
+	for(int i = 1; i<=n; i++)
+	{
+		sum += i * ((n - (i - 1)) + (n - i));
+	}
+
+	return sum;
+}
+
+unsigned long long Funij::sumax(unsigned long long n)
+{
+	unsigned long long sum = 0;
+
+
+	return sum;
+}
+
+unsigned long long Funij::sumsum(unsigned long long n)
+{
+	unsigned long long sum = 0;
+
+
+	return sum;
+}
 
 int main()
 {
+	unsigned long long r01 = Funij::sumin(6);
+	unsigned long long r02 = Funij::sumin(45);
+	unsigned long long r03 = Funij::sumin(999);
+	unsigned long long r04 = Funij::sumin(5000);
+
+	unsigned long long r05 = Funij::sumax(6);
+	unsigned long long r06 = Funij::sumax(45);
+	unsigned long long r07 = Funij::sumax(999);
+	unsigned long long r08 = Funij::sumax(5000);
+
+	unsigned long long r09 = Funij::sumsum(6);
+	unsigned long long r10 = Funij::sumsum(45);
+	unsigned long long r11 = Funij::sumsum(999);
+	unsigned long long r12 = Funij::sumsum(5000);
+
 	return 0;
 }
