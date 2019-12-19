edf5d3ecedee5b3d26b8948f4c49fcdab50abca8
DaeGuk Do <eornrsayclub@gmail.com>
2019-12-19 10:33:48 +0900

    c# study other process execution in original process

---
+++ 02_C_sharp/004_otherProcessExec/01_otherProcessExec.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace otherProcessExec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (System.Diagnostics.Process dialogProcess = new System.Diagnostics.Process())
+                {
+                    dialogProcess.StartInfo.FileName = ".\\resource\\dialog.exe";
+
+                    dialogProcess.Start();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
