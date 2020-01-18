f09ff03cb37de2d3b107e745813dc8f7172408be
ddk <eornrsayclub@gmail.com>
2020-01-19 00:33:47 +0900

    c++ study codewar : edit test code (Are they the "same"?)

--- 03_C_plusplus/040_Are they the same/01_Are_they_the_same.cpp
+++ 03_C_plusplus/040_Are they the same/01_Are_they_the_same.cpp
@@ -43,6 +43,7 @@ a or b are empty or not empty lists.
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <math.h>
 
 using namespace std;
 
@@ -54,44 +55,48 @@ class Same
 
 bool Same::comp(vector<int> target, vector<int> square)
 {
+	if(target.empty() || square.empty())
+	{
+		return false;
+	}
+
 	sort(target.begin(), target.end());
 	sort(square.begin(), square.end());
 
 	int capacity = target.capacity();
-	int countChecker = 0;
 
 	for(int i = 0; i < capacity; i++)
 	{
-		if((target[i] == NULL) || (square[i] == NULL))
+		if(target[i] == 0 || square[i] == 0)
 		{
 			return false;
 		}
 
-		if((target[i] * target[i]) == square[i])
+		if(!count(square.begin(),square.end(), target[i] * target[i]))
 		{
-			countChecker++;
+			return false;
+		}
+
+		if(!count(target.begin(), target.end(), (int)sqrt(square[i])))
+		{
+			return false;
 		}
 	}
 
-	if((countChecker == capacity) && (capacity != 0) && (!target.empty()))
-	{
-		return true;
-	}
-	else
+	if(capacity == 0)
 	{
 		return false;
 	}
+
+	return true;
 }
 
 int main()
 {
-	// a = [121, 144, 19, 161, 19, 144, 19, 11]  
-    // b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
-
-	static const int arr1[] = {NULL};
+	static const int arr1[] = {64, 38, 44, 63, 56, 70, 98, 19, 93, 53, 21, 67, 45, 21, 99, 8, 42};
     vector<int> vec1 (arr1, arr1 + sizeof(arr1) / sizeof(arr1[0]) );
 
-	static const int arr2[] = {NULL};
+	static const int arr2[] = {4096, 1444, 1936, 3969, 3136, 4900, 9604, 361, 8649, 2809, 441, 4489, 2025, 441, 9801, 64, 1764};
     vector<int> vec2 (arr2, arr2 + sizeof(arr2) / sizeof(arr2[0]) );
 
 	bool r = Same::comp(vec1, vec2);
