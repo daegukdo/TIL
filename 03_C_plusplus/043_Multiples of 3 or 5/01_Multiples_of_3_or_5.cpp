43082a462dce8643dc0dcee22cab370bfdc9cd08
DaeGuk Do <eornrsayclub@gmail.com>
2020-01-22 16:07:20 +0900

    c++ study codewar : add issue description (Multiples of 3 or 5)

---
+++ 03_C_plusplus/043_Multiples of 3 or 5/01_Multiples_of_3_or_5.cpp
@@ -0,0 +1,34 @@
+// c++ code-war
+/*
+issue description
+
+Multiples of 3 or 5
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+
+The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+
+Courtesy of ProjectEuler.net
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int solution(int number);
+
+int main()
+{
+
+	return 0;
+}
+
+int solution(int number) 
+{
+
+}
