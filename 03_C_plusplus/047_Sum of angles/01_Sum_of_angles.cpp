715c43238ce35e5fc482680a31ad05d2e1eb29dd
ddk <eornrsayclub@gmail.com>
2020-02-09 22:07:39 +0900

    c++ study codewar : add my solution code (Sum of angles)

---
+++ 03_C_plusplus/047_Sum of angles/01_Sum_of_angles.cpp
@@ -0,0 +1,21 @@
+// c++ code-war
+/*
+issue description
+
+Sum of angles
+
+Find the total sum of angles in an n sided shape. N will be greater than 2.
+*/
+
+int angle(int n);
+
+int main()
+{
+
+	return 0;
+}
+
+int angle(int n) 
+{
+	return 180 * (n-2);
+}
