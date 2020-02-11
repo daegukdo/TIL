8c2e7f70c7a42bbffb93104a0fdd99d6192752d6
DaeGuk Do <eornrsayclub@gmail.com>
2020-02-11 14:20:45 +0900

    c++ study codewar : add issue description (Create Phone Number)

---
+++ 03_C_plusplus/049_Create Phone Number/01_Create_Phone_Number.cpp
@@ -0,0 +1,30 @@
+// c++ code-war
+/*
+issue description
+
+Create Phone Number
+
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+Example:
+createPhoneNumber(int[10]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}) // => returns "(123) 456-7890"
+The returned format must be correct in order to complete this challenge.
+Don't forget the space after the closing parentheses!
+*/
+
+#include <string>
+
+using namespace std;
+
+std::string createPhoneNumber(const int arr [10]);
+
+int main()
+{
+
+	return 0;
+}
+
+std::string createPhoneNumber(const int arr [10])
+{
+	return;
+}
