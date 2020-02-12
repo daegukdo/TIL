44422c685c53a0724165c08a07b3c78e948d75d2
ddk <eornrsayclub@gmail.com>
2020-02-12 22:32:18 +0900

    c++ study codewar : edit my solution code and answer of other person (Create Phone Number)

--- 03_C_plusplus/049_Create Phone Number/02_Create_Phone_Number_answer.cpp
+++ 03_C_plusplus/049_Create Phone Number/02_Create_Phone_Number_answer.cpp
@@ -28,33 +28,10 @@ int main()
 	return 0;
 }
 
-std::string createPhoneNumber(const int arr [10])
-{
-	string arrStr[10];
+#include <string>
 
-	for(int i = 0; i < 10; i++)
-	{
-		arrStr[i] = std::to_string(arr[i]);
-	}
-
-	string firstPnStr = "";
-	string secondPnStr = "";
-	string thirdPnStr = "";
-
-	for(int i = 0; i < 3; i++)
-	{
-		firstPnStr += arrStr[i];
-	}
-
-	for(int i = 3; i < 6; i++)
-	{
-		secondPnStr += arrStr[i];
-	}
-
-	for(int i = 6; i < 10; i++)
-	{
-		thirdPnStr += arrStr[i];
-	}
-
-	return "(" + firstPnStr + ") " + secondPnStr + "-" + thirdPnStr;
+std::string createPhoneNumber(const int arr [10]){
+  char buf[15];
+  snprintf(buf, sizeof(buf), "(%d%d%d) %d%d%d-%d%d%d%d%d", arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7], arr[8], arr[9]);
+  return buf;
 }
