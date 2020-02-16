8954c29b56274cd59211b3558a6149632bd5c6fd
ddk <eornrsayclub@gmail.com>
2020-02-16 22:43:54 +0900

    c++ study codewar : add issue description (Fun with trees: max sum)

---
+++ 03_C_plusplus/052_Fun with trees max sum/01_Fun_with_trees_max_sum.cpp
@@ -0,0 +1,71 @@
+// c++ code-war
+/*
+issue description
+
+Fun with trees: max sum
+
+You are given a binary tree. Implement the method maxSum which returns the maximal sum of a route from root to leaf. 
+
+For example, given the following tree:
+
+    17
+   /  \
+  3   -10
+ /    /  \
+2    16   1
+         /
+        13
+The method should return 23, since [17,-10,16] is the route from root to leaf with the maximal sum.
+
+The class TreeNode is available for you:
+
+class TreeNode
+{
+    public:
+        TreeNode* left;
+        TreeNode* right;
+        int value;
+    ...
+};
+This kata is part of fun with trees series:
+
+Fun with trees: max sum
+Fun with trees: array to tree
+Fun with trees: is perfect
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class TreeNode
+{
+public:
+	TreeNode* left;
+	TreeNode* right;
+	int value;
+};
+
+class Solution
+{
+public:
+	static int maxSum(TreeNode* root)
+	{
+		// TODO: implementation
+		return -1;
+    }
+};
+
+int main()
+{
+	// 변수를 셋업하는 함수들이 필요?
+	// 포인터 변수에 값을 정적 함수로 셋업?
+	/*
+	TreeNode* left = TreeNode::leaf(-22)->withLeaves(9, 50);
+    TreeNode* right = TreeNode::leaf(11)->withLeaves(9, 2);
+    TreeNode* root = TreeNode::join(5, left, right);
+	*/
+
+	return 0;
+}
+
