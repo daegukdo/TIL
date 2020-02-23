07397f1214b782cfda35ce02c068580cf0895e6d
ddk <eornrsayclub@gmail.com>
2020-02-23 23:15:58 +0900

    c++ study codewar : add issue description (String ends with?)

---
+++ 03_C_plusplus/055_String ends with/01_String_ends_with.cpp
@@ -0,0 +1,32 @@
+// c++ code-war
+/*
+issue description
+
+String ends with?
+
+Description
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+*/
+
+#include <string>
+
+using namespace std;
+
+bool solution(std::string const &str, std::string const &ending);
+
+int main()
+{
+
+	return 0;
+}
+
+bool solution(std::string const &str, std::string const &ending)
+{
+
+    return true;
+}
