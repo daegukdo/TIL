995c168810d62dae38fa8fc9f04d81e3b58d4660
DaeGuk Do <eornrsayclub@gmail.com>
2020-03-04 15:47:29 +0900

    c++ study codewar : add my test code (Remove duplicate words)

--- 03_C_plusplus/061_Remove duplicate words/01_Remove_duplicate_words.cpp
+++ 03_C_plusplus/061_Remove duplicate words/01_Remove_duplicate_words.cpp
@@ -20,20 +20,52 @@ Output:
 */
 
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-std::string removeDuplicateWords(const std::string& str);
+std::string removeDuplicateWords(const string& str);
 
 int main()
 {
+	string r1 = removeDuplicateWords("my cat is cat fat");
 
 	return 0;
 }
 
-std::string removeDuplicateWords(const std::string& str)
+std::string removeDuplicateWords(const string& str)
 {
+	string result = "";
 
-    return ;
+	string inputStr = str;
+
+	istringstream iss(inputStr);
+
+	vector<string> vctStr;
+
+	for(std::string s; iss >> s; )
+	{
+		if(find(vctStr.begin(), vctStr.end(), s) != vctStr.end())
+		{
+			// do nothing
+		}
+		else
+		{
+			vctStr.push_back(s);
+		}
+	}
+
+	for(int i = 0; i < vctStr.capacity(); i++)
+	{
+		if(i != 0)
+		{
+			result += " ";
+		}
+
+		result += vctStr[i];
+	}
+
+    return result;
 }
