8c31b8e74648596b2bbd475de7bf7967f4f229ef
DaeGuk Do <eornrsayclub@gmail.com>
2020-03-04 12:42:15 +0900

    c++ study codewar : add issue description (Remove duplicate words)

---
+++ 03_C_plusplus/061_Remove duplicate words/01_Remove_duplicate_words.cpp
@@ -0,0 +1,39 @@
+// c++ code-war
+/*
+@ issue description
+
+Remove duplicate words
+
+@ Description
+
+Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+
+Example:
+
+Input:
+
+'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+
+Output:
+
+'alpha beta gamma delta'
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+std::string removeDuplicateWords(const std::string& str);
+
+int main()
+{
+
+	return 0;
+}
+
+std::string removeDuplicateWords(const std::string& str)
+{
+
+    return ;
+}
