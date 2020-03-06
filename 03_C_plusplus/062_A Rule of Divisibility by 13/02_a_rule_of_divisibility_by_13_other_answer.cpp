5f1db9af7aad2ec29ceee7ca7a433544a412c5b4
DaeGuk Do <eornrsayclub@gmail.com>
2020-03-06 12:46:55 +0900

    c++ study codewar : edit my solution code and answer of other person (a rule of divisibility by 13)

--- 03_C_plusplus/062_A Rule of Divisibility by 13/02_a_rule_of_divisibility_by_13_other_answer.cpp
+++ 03_C_plusplus/062_A Rule of Divisibility by 13/02_a_rule_of_divisibility_by_13_other_answer.cpp
@@ -52,49 +52,14 @@ public:
     static long long thirt(long long n);
 };
 
-long long Thirteen::thirt(long long n)
-{
-	long long nInput = n;
-
-	long long divisionsList [6] = {1, 10, 9, 12, 3, 4};
-	int divCount = 0;
-
-	string nData = to_string(nInput);
-	int lengthNData = nData.length();
-
-	long long nTmp = 0;
-	bool looper = true;
-
-	while(true)
-	{
-		for(int i = lengthNData-1; i >= 0; i--)
-		{
-			nTmp += (nData[i] - '0') * divisionsList[divCount];
-			
-			divCount++;
-
-			if(divCount == 6)
-			{
-				divCount = 0;
-			}
-		}
-
-		if(nInput != nTmp)
-		{
-			nInput = nTmp;
-		}
-		else
-		{
-			break;
-		}
-
-		nTmp = 0;
-		divCount = 0;
-		nData = to_string(nInput);
-	    lengthNData = nData.length();
-	}
-
-	return nInput;
+long long Thirteen::thirt(long long n){
+  int divs[6] = {1, 10, 9, 12, 3, 4};
+  while(n >= 100){
+    long long aux = n;
+    n = 0;
+    for(int i = 0; aux != 0; n += (aux%10)*divs[i%6], aux /= 10, i++);
+  }
+  return n;
 }
 
 int main()
