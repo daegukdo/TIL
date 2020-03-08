20a586018cb7b16cdc9ac8af4777ef1ca63fe9df
ddk <eornrsayclub@gmail.com>
2020-03-08 23:32:14 +0900

    c++ study codewar : add my test code (Perimeter of squares in a rectangle)

--- 03_C_plusplus/064_Perimeter of squares in a rectangle/01_Perimeter_of_squares_in_a_rectangle.cpp
+++ 03_C_plusplus/064_Perimeter of squares in a rectangle/01_Perimeter_of_squares_in_a_rectangle.cpp
@@ -14,6 +14,8 @@ Could you give the sum of the perimeters of all the squares in a rectangle when 
 
 alternative text
 
+ref : 01_img.png
+
 #Hint: See Fibonacci sequence
 
 #Ref: http://oeis.org/A000045
@@ -33,13 +35,49 @@ typedef unsigned long long ull;
 
 class SumFct
 {
-  public:
-  static ull perimeter(int n);
+public:
+	static ull perimeter(int n);
 };
 
+ull SumFct::perimeter(int n)
+{
+	// Fibonacci sequence : 1 1 2 3 5 8 13 21 34 ... //
+
+	// define Fib. variable ... dynamic variable
+	ull step = n+1;
+	ull *numArray = new ull[step]();
+
+	// first setup step
+	numArray[0] = 1;
+	numArray[1] = 1;
+
+	// iter. step
+	for(int i = 2; i < step; i++)
+	{
+		numArray[i] = numArray[i-1] + numArray[i-2];
+	}
+
+	// cal. rect. area //
+
+	// define rectArea variable
+	ull rectArea = 0;
+
+	// cal.
+	for(int i = 0; i < step; i++)
+	{
+		rectArea += numArray[i];
+	}
+
+	rectArea *= 4;
+
+	// del. Fib. variable ... dynamic variable
+	delete[] numArray;
+
+	return rectArea;
+}
 
 int main()
 {
-
+	ull r1 = SumFct::perimeter(5);
 	return 0;
 }
