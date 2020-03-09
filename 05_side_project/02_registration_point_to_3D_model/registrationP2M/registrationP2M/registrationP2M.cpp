09aae79a19b1c8b6834ea16d71fb9670ea41c650
DaeGuk Do <eornrsayclub@gmail.com>
2020-03-09 19:30:14 +0900

    side project : registration point to 3D model read raw point data

--- 05_side_project/02_registration_point_to_3D_model/registrationP2M/registrationP2M/registrationP2M.cpp
+++ 05_side_project/02_registration_point_to_3D_model/registrationP2M/registrationP2M/registrationP2M.cpp
@@ -1,3 +1,12 @@
+// for test
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+// for registration
 #include "registrationP2M.h"
 // #pragma comment(lib, "ANN.lib")
 
@@ -5,11 +14,141 @@
 
 namespace registrationP2M
 {
+	//////////////////////////////////////////////////////////////////////////////
+	// CPointSet
+	//////////////////////////////////////////////////////////////////////////////
+	CPointSet::CPointSet()
+	{
+		m_iNumPoints = 0;
+		m_pData = NULL;
+	}
 
+	CPointSet::CPointSet(int iNumPoints)
+	{
+		m_iNumPoints = iNumPoints;
+		m_pData = new double[m_iNumPoints*DIM];
+		memset(m_pData, 0, m_iNumPoints*DIM*sizeof(double));
+	}
+
+	CPointSet::CPointSet(const CPointSet& other)
+	{
+		m_iNumPoints = other.m_iNumPoints;
+		m_pData = new double[m_iNumPoints*DIM];
+		memcpy(m_pData, other.m_pData, m_iNumPoints*DIM*sizeof(double));
+	}
+
+	CPointSet::CPointSet(int iNumPoints, double* pPoints)
+	{
+		m_iNumPoints = iNumPoints;
+		m_pData = new double[m_iNumPoints*DIM];
+		memcpy(m_pData, pPoints, m_iNumPoints*DIM*sizeof(double));
+	}
+
+	CPointSet::~CPointSet()
+	{
+		if(m_pData != NULL)
+			delete [] m_pData;
+	}
+
+	bool CPointSet::SetPointSet(double* pPoints)
+	{
+		if (m_pData != NULL)
+		{
+			memcpy(m_pData, pPoints, m_iNumPoints*DIM*sizeof(double));
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	CPointSet CPointSet::GetPointSet(int iIndex) const
+	{
+		if(iIndex < m_iNumPoints)
+		{
+			double* pPoints = new double[DIM];
+			memcpy(pPoints, m_pData+DIM*iIndex, DIM*sizeof(double));
+			CPointSet ret(1, pPoints);
+			delete [] pPoints;
+
+			return ret;
+		}
+		else
+			return CPointSet();
+	}
+
+	CPointSet CPointSet::GetPointSet(vector<int> vIndex) const
+	{
+		int i;
+		int iNumPoints = vIndex.size();
+		double* pPoints = new double[iNumPoints*DIM];
+		for(i = 0; i < iNumPoints; i++)
+		{
+			if(vIndex[i] < m_iNumPoints)
+				memcpy(pPoints+DIM*i, m_pData+DIM*vIndex[i], DIM*sizeof(double));
+			else
+			{
+				iNumPoints = 0;
+				delete [] pPoints;
+				pPoints = NULL;
+				break;
+			}
+		}
+
+		CPointSet ret(iNumPoints, pPoints);
+		if(pPoints != NULL) delete [] pPoints;
+
+		return ret;
+	}
 }
 
-void main()
+int main()
 {
+	// read raw point data .txt
+	vector<double> pointData;
 
-	return;
+	string filePath = "data\\_CollectedPointsFemur.txt";
+
+	ifstream openFile(filePath);
+
+	if( openFile.is_open() )
+	{
+		string line;
+
+		while(getline(openFile, line))
+		{
+			stringstream ss(line);
+			int count = 0;
+
+			while(ss.good())
+			{
+				string substr;
+                getline( ss, substr, ',' );
+
+				if(count == 14 || count == 13 || count ==12)
+				{
+					pointData.push_back(stod(substr));
+				}
+
+				count++;
+			}
+		}
+
+		openFile.close();
+	}
+
+	// cal.
+	double* m_pData_ = new double[pointData.capacity()];
+
+	for(int i = 0; i < pointData.capacity(); i++)
+	{
+		m_pData_[i] = pointData[i];
+	}
+
+	delete[] m_pData_;
+
+
+	// fin.
+	return 0;
 }
--- 05_side_project/02_registration_point_to_3D_model/registrationP2M/registrationP2M/registrationP2M.h
+++ 05_side_project/02_registration_point_to_3D_model/registrationP2M/registrationP2M/registrationP2M.h
@@ -16,4 +16,24 @@ namespace registrationP2M
 	//////////////////////////////////////////////////////////////////////////////
 	//	constants
 	static const double ZERO_PRECISION = 1e-4;
+
+	class CPointSet
+	{
+	protected:
+		int m_iNumPoints;
+		double* m_pData;		// size: m_iNumPoints * DIM_POINT
+
+	public:
+		static const int DIM = 3;
+
+		CPointSet();
+		CPointSet(int iNumPoints);
+		CPointSet(const CPointSet& other);
+		CPointSet(int iNumPoints, double* pPoints);
+		~CPointSet();
+
+		bool SetPointSet(double* pPoints);
+		CPointSet GetPointSet(int iIndex) const;
+		CPointSet GetPointSet(vector<int> vIndex) const;
+	};
 }
