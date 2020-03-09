bc8c4fa6f47fa95a40c9d398b0328c7929dce236
DaeGuk Do <eornrsayclub@gmail.com>
2020-03-09 12:57:39 +0900

    side project : registration point to 3D model add VS2012 c++ project

---
+++ 05_side_project/02_registration_point_to_3D_model/registrationP2M/registrationP2M/registrationP2M.cpp
@@ -0,0 +1,15 @@
+#include "registrationP2M.h"
+// #pragma comment(lib, "ANN.lib")
+
+#include <array>
+
+namespace registrationP2M
+{
+
+}
+
+void main()
+{
+
+	return;
+}
---
+++ 05_side_project/02_registration_point_to_3D_model/registrationP2M/registrationP2M/registrationP2M.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <memory>
+#include <cmath>
+#include <vector>
+#include <list>
+#include <utility>
+#include <algorithm>
+// #include <ANN.h>
+// #include <Eigen/Eigen>
+#include <deque>
+
+using namespace std;
+
+namespace registrationP2M
+{
+	//////////////////////////////////////////////////////////////////////////////
+	//	constants
+	static const double ZERO_PRECISION = 1e-4;
+}
