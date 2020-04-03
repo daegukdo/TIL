1ebcba80052a4c3edf069c40cb7763e384c75b09
DaeGuk Do <eornrsayclub@gmail.com>
2020-04-03 13:10:02 +0900

    side project : point generation based STL edit module for gen. points (bugfix indexing)

--- 05_side_project/04_point_gen_in_STL/02_modulization/01_func_for_gen_points.py
+++ 05_side_project/04_point_gen_in_STL/02_modulization/01_func_for_gen_points.py
@@ -10,22 +10,22 @@ def select_random_vertex_index(point40_on_STL, vertex_data):
     selected_index = [i0, i1, ... in]
     """
     
-    vertex_group = []
+    vertex_group                  = []
 
     for i in range(len(point40_on_STL)):
-        temp = []
+        temp                      = []
         for j in range(len(vertex_data)):
             if point40_on_STL[i] in vertex_data[j]:
                 temp.append(j)
         vertex_group.append(temp)
 
-    vertex_selected_index = []
+    vertex_selected_index         = []
 
     for i in range(len(vertex_group)):
-        randomIndex = random.choice(vertex_group[i])
+        randomIndex               = random.choice(vertex_group[i])
         vertex_selected_index.append(randomIndex)
         
-    return vertex_selected_index
+    return vertex_selected_index, vertex_group
 
 def random_vector(length = 1):
     """
@@ -45,6 +45,7 @@ def random_vector(length = 1):
     y              = np.sin(theta) * np.sin(phi)  # Cartesian coordinate y
     z              = np.cos(phi)                  # Cartesian coordinate z
     vector         = [x*math.sqrt(length), y*math.sqrt(length), z*math.sqrt(length)]
+
     return vector
 
 def add_mm_error_to_point(point, error = 20):
@@ -56,13 +57,13 @@ def add_mm_error_to_point(point, error = 20):
     point_added_error = [x', y', z']
     """
     
-    point_added_error = point.copy()
+    point_added_error      = point.copy()
     
-    xyz_error = random_vector(error)
+    xyz_error              = random_vector(error)
 
-    point_added_error[0] += xyz_error[0]
-    point_added_error[1] += xyz_error[1]
-    point_added_error[2] += xyz_error[2]
+    point_added_error[0]   += xyz_error[0]
+    point_added_error[1]   += xyz_error[1]
+    point_added_error[2]   += xyz_error[2]
     
     return point_added_error
 
@@ -78,18 +79,46 @@ def find_projection_point_in_plane_3D(point, plane, norm_vect):
     False -> value of diff. with plane = float(d)
     """
     
-    plane_ABC_sqrt = math.sqrt(plane[0]*plane[0] + plane[1]*plane[1] + plane[2]*plane[2])
-    dist = (plane[3] + plane[0] * point[0] + plane[1] * point[1] + plane[2] * point[2]) / plane_ABC_sqrt
-    projected_point = point - dist * norm_vect
+    plane_ABC_sqrt       = math.sqrt(plane[0]*plane[0] + plane[1]*plane[1] + plane[2]*plane[2])
+    dist                 = (plane[3] + plane[0] * point[0] + plane[1] * point[1] + plane[2] * point[2]) / plane_ABC_sqrt
+    # print(dist)
+    projected_point      = point - dist * norm_vect
     
-    on_the_plane_value = plane[3] + plane[0] * projected_point[0] + plane[1] * projected_point[1] + plane[2] * projected_point[2]
+    on_the_plane_value   = is_including_point(plane, projected_point)
     
-    if 0.0001 > on_the_plane_value:
+    if on_the_plane_value:
         pass
     else:
-        print(on_the_plane_value)
+        print(projected_point)
+        # print(on_the_plane_value)
      
-    return projected_point   
+    return projected_point 
+
+def is_including_point(plane, point):
+    """
+    input data structure
+    point = [x, y, z]
+    plane = [a, b, c, d] # plane eq. = ax + by + cz + d = 0
+    
+    output data structure
+    is point included in plane = bool
+    """
+    
+    a                 = plane[0]
+    b                 = plane[1]
+    c                 = plane[2]
+    d                 = plane[3]
+    x                 = point[0]
+    y                 = point[1]
+    z                 = point[2]
+    
+    included_value    = a*x + b*y + c*z + d
+    # print(included_value)
+    
+    if included_value < 0.00001:
+        return True
+    else:
+        return False
     
 def cal_points40_with_error_on_plane(point40_on_STL, plane_selected, vector_selected):
     """
@@ -102,23 +131,37 @@ def cal_points40_with_error_on_plane(point40_on_STL, plane_selected, vector_sele
     points_on_plane = [[xpr0, ypr0, zpr0], ... [xprn, yprn, zprn]]
     """
     
-    points40_with_error_on_plane = []
+    points40_with_error_on_plane    = []
 
-    data_point_40_slt = point40_on_STL.copy()
+    data_point_40_slt               = point40_on_STL.copy()
 
-    for i in range(len(data_point_40_slt)):
-        _point_added_error = add_mm_error_to_point(data_point_40_slt[i])
-        _point_on_plane = find_projection_point_in_plane_3D(_point_added_error, 
-                                                            plane_selected[i],
-                                                            vector_selected[i])
+    for i in range(len(data_point_40_slt)):        
+        _point_added_error          = add_mm_error_to_point(data_point_40_slt[i])
+        _point_on_plane             = find_projection_point_in_plane_3D(_point_added_error, 
+                                                                        plane_selected[i],
+                                                                        vector_selected[i])
         
         if np.isnan(np.sum(_point_on_plane)):
-            print(_point_on_plane)
             points40_with_error_on_plane.append(data_point_40_slt[i])
         else:
             points40_with_error_on_plane.append(_point_on_plane)
             
     return points40_with_error_on_plane
+
+def get_dist_btw_2_3Dpoints(point1, point2):
+    """
+    input data structure
+    point1 = [x1, y1, z1]
+    point2 = [x2, y2, z2]
+    
+    output data structure
+    distance
+    """
+    
+    sum        = 0
+    for i in range(3):
+        sum    = sum + (point1[i] - point2[i])**2
+    return math.sqrt(sum)
 
 def get_closet_points(ref_points, trg_points):
     """
@@ -130,11 +173,11 @@ def get_closet_points(ref_points, trg_points):
     closet_points = [[xc0, yc0, zc0], ... [xcn, ycn, zcn]]
     """
     
-    tree = KDTree(trg_points)
-    nearest_dist, nearest_ind = tree.query(ref_points, k=2)
+    tree                         = KDTree(trg_points)
+    nearest_dist, nearest_ind    = tree.query(ref_points, k=2)
     
-    nearest_index = nearest_ind[:,1]
-    closet_points = []
+    nearest_index                = nearest_ind[:,1]
+    closet_points                = []
     
     for i in range(len(nearest_index)):
         closet_points.append(trg_points[nearest_index[i]])
@@ -164,7 +207,7 @@ def get_data_with_index(index_data, some_data):
     some_data_indexed = []
     
     for i in range(len(index_data)):
-        some_data_indexed.append(some_data[i])
+        some_data_indexed.append(some_data[index_data[i]])
         
     return some_data_indexed
     
@@ -179,8 +222,8 @@ def add_error_to_point_following_to_vector_dirct(point, error_length, vector):
     point_added_error_to_vector_direction = [xe, ye, ze]
     """
     
-    point_with_error = []
-    unit_error = []
+    point_with_error   = []
+    unit_error         = []
     
     for i in range(len(point)):
         unit_error.append(error_length * vector[i])
