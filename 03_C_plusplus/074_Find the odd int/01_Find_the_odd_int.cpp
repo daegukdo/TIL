f9fab7a58c70a498653a7c772a1bdc44373dc594
ddk <eornrsayclub@gmail.com>
2020-05-07 21:47:31 +0900

    c++ study codewar : add issue description (Find the odd int)

---
+++ 03_C_plusplus/074_Find the odd int/01_Find_the_odd_int.cpp
@@ -0,0 +1,29 @@
+// c++ code-war
+/*
+@ issue description
+
+Find the odd int
+
+@ Description
+
+Given an array, find the integer that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+*/
+
+#include <vector>
+
+using namespace std;
+
+int findOdd(const std::vector<int>& numbers);
+
+int main()
+{
+
+	return 0;
+}
+
+int findOdd(const std::vector<int>& numbers)
+{
+    //your code here
+}
