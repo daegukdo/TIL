465edf7c05896431bfa934ebc6567e11a33b27d2
Daeguk Do <38026518+daegukdo@users.noreply.github.com>
2020-05-16 23:36:51 +0900

    Create 01_Simple_Pig_Latein.cpp
    C++ study
    Add issue description (Simple Pig Latin)

---
+++ 03_C_plusplus/077_Simple Pig Latin/01_Simple_Pig_Latein.cpp
@@ -0,0 +1,32 @@
+/* 
+@ issue description
+Simple Pig Latin
+
+Move the first letter of each word to the end of it, then add "ay" to the end of the word. 
+
+Leave punctuation marks untouched.
+
+Examples
+
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+pigIt('Hello world !');     // elloHay orldway !
+
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+std::string pig_it(std::string str);
+
+int main() 
+{
+  
+	std::cout << "j" + "\n";
+}
+
+std::string pig_it(std::string str)
+{
+    // Code here
+}
