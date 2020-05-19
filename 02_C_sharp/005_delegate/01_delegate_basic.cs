a9f2c3634049cc82b313ff20db06bebfc49b9aae
DaeGuk Do <eornrsayclub@gmail.com>
2020-05-19 12:58:08 +0900

    C# study add "delegate" example code

---
+++ 02_C_sharp/005_delegate/01_delegate_basic.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    // define delegate
+    delegate int MyDelegate(int a, int b);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator Calc = new Calculator();
+            MyDelegate CallBack;
+
+            CallBack = new MyDelegate(Calc.Plus);
+            Console.WriteLine(CallBack(3, 4));
+
+            CallBack = new MyDelegate(Calc.Minus);
+            Console.WriteLine(CallBack(4, 3));
+
+            return;
+        }
+    }
+
+    class Calculator
+    {
+        public int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
