4a8ccba4cf16a1f49467f03fd509b4ebb290b7e4
DaeGuk Do <eornrsayclub@gmail.com>
2020-05-25 19:54:53 +0900

    C# study add "property" example codes

---
+++ 02_C_sharp/007_property/001_property_example.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace test
+{
+    // define class with property
+    class NameCard
+    {
+        private int age;
+        private string name;
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                age = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NameCard MyCard = new NameCard();
+
+            MyCard.Age = 24;
+            MyCard.Name = "sanghyun";
+
+            Console.WriteLine("age : {0}", MyCard.Age);
+            Console.WriteLine("name : {0}", MyCard.Name);
+
+            return;
+        }
+    }
+}
