614e6a654c18fd8ae3ba78066f3af777a0004531
DaeGuk Do <eornrsayclub@gmail.com>
2020-06-05 17:41:37 +0900

    C# study add project for thread with UI

---
+++ 02_C_sharp/009_WpfUIThread/WpfUIThread/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfUIThread
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            testWhile();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate
+            {
+                testWhile();
+            }));
+        }
+
+        private void testWhile()
+        {
+            long a = 0;
+            bool grap = true;
+
+            while (grap)
+            {
+                a = a + 1;
+
+                if (a > 100000000000000)
+                {
+                    grap = false;
+                }
+            }
+        }
+    }
+}
