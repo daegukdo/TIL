c887be8f1073b4cbb9c2e56fb855009abefbe0f0
DaeGuk Do <eornrsayclub@gmail.com>
2020-06-08 12:59:38 +0900

    C# study edit project for thread with UI

--- 02_C_sharp/009_WpfUIThread/WpfUIThread/MainWindow.xaml.cs
+++ 02_C_sharp/009_WpfUIThread/WpfUIThread/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.ComponentModel;
 
 namespace WpfUIThread
 {
@@ -21,25 +22,45 @@ namespace WpfUIThread
     /// </summary>
     public partial class MainWindow : Window
     {
+        public class CheckProcessFlag : INotifyPropertyChanged
+        {
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            private void NotifyPropertyChanged()
+            {
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("fin"));
+                }
+            }
+
+            public bool SetProcessTgFlag
+            {
+                get { return ProcessTgFlag; }
+                set
+                {
+                    ProcessTgFlag = value;
+
+                    NotifyPropertyChanged();
+                }
+            }
+
+            private bool ProcessTgFlag = false;
+        }
+
+        CheckProcessFlag checkProcessFlag;
+        BackgroundWorker threadProcess = new BackgroundWorker();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            checkProcessFlag = new CheckProcessFlag();
+            checkProcessFlag.SetProcessTgFlag = false;
+            checkProcessFlag.PropertyChanged += new PropertyChangedEventHandler(processFin);
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            testWhile();
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate
-            {
-                testWhile();
-            }));
-        }
-
-        private void testWhile()
         {
             long a = 0;
             bool grap = true;
@@ -48,11 +69,78 @@ namespace WpfUIThread
             {
                 a = a + 1;
 
-                if (a > 100000000000000)
+                if (a > 300000000)
                 {
                     grap = false;
                 }
             }
+
+            if (checkProcessFlag.SetProcessTgFlag == true)
+            {
+                checkProcessFlag.SetProcessTgFlag = false;
+            }
+            else
+            {
+                checkProcessFlag.SetProcessTgFlag = true;
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            threadProcess.DoWork += new DoWorkEventHandler(testWhileWithSync);
+            threadProcess.WorkerSupportsCancellation = false;
+            threadProcess.RunWorkerAsync();
+        }
+
+        private void testWhileWithSync(object sender, DoWorkEventArgs e)
+        {
+            long a = 0;
+            bool grap = true;
+
+            while (grap)
+            {
+                a = a + 1;
+
+                if (a > 300000000)
+                {
+                    grap = false;
+                }
+            }
+
+            if (checkProcessFlag.SetProcessTgFlag == true)
+            {
+                checkProcessFlag.SetProcessTgFlag = false;
+            }
+            else
+            {
+                checkProcessFlag.SetProcessTgFlag = true;
+            }
+        }
+
+        private void processFin(object sender, PropertyChangedEventArgs e)
+        {
+            Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate
+            {
+                if (heartToggle.IsChecked == true)
+                {
+                    heartToggle.IsChecked = false;
+                }
+                else
+                {
+                    heartToggle.IsChecked = true;
+                }
+
+                if (smileToggle.IsChecked == true)
+                {
+                    smileToggle.IsChecked = false;
+                }
+                else
+                {
+                    smileToggle.IsChecked = true;
+                }
+            }));
+
+            threadProcess.DoWork -= new DoWorkEventHandler(testWhileWithSync);
         }
     }
 }
