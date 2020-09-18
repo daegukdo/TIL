9275e8c469f37550d717b33e8aa954f119470526
Daeguk Do <38026518+daegukdo@users.noreply.github.com>
2020-09-18 23:02:51 +0900

    Update 01_Diagonal_Traverse.cpp
    C++ study
    LeetCode : edit source code (Diagonal Traverse)

--- 03_C_plusplus/086_Diagonal Traverse/01_Diagonal_Traverse.cpp
+++ 03_C_plusplus/086_Diagonal Traverse/01_Diagonal_Traverse.cpp
@@ -23,6 +23,7 @@ The total number of elements of the given matrix will not exceed 10,000.
 ref : https://leetcode.com/explore/learn/card/array-and-string/202/introduction-to-2d-array/1167/
 */
 
+#include <iostream>
 #include <vector>
 #include <algorithm>
 
@@ -31,83 +32,73 @@ using namespace std;
 class Solution 
 {
 public:
-    vector<int> findDiagonalOrder(vector<vector<int>>& matrix) 
+  vector<int> findDiagonalOrder(vector<vector<int>>& matrix) 
 	{
-        vector<int> diagonalTraverseResult;
-		
-		bool iter = true;
-		bool isUp = true;
-		bool isReduce = false;
-
-		int checker = 0;
-
-		int tracerM = 0;
-		int tracerN = 0;
-		
-		while(iter)
-		{
-			// cal. each diagonal line   //////////
-			vector<int> tmpVct;
-
-			if(isReduce)
-			{
-				checker--;
-			}
-			else
-			{
-				checker++;
-			}
-
-			if(checker % 2 == 1)
-			{
-				isUp = true;
-			}
-			else
-			{
-				isUp = false;
-			}
-
-			int m = tracerM;
-			int n = tracerN;
-
-			for(int i = 0; i < checker; i++)
-			{
-				tmpVct.push_back(matrix[m][n]);
-
-				m++;
-				n--;
-			}
-
-			if(tracerN != matrix.size() - 1)
-			{
-			    tracerN++;
-			}
-			else
-			{
-				tracerM++;
-
-				if(!isReduce)
-				{
-				    isReduce = true;
-				}
-			}
-			// end cal. each diagonal line //////////
-
-			if(isUp)
-			{
-				reverse(tmpVct.begin(), tmpVct.end());
-			}
-
-			diagonalTraverseResult.insert(diagonalTraverseResult.end(), tmpVct.begin(), tmpVct.end());
-
-			if(diagonalTraverseResult.size() == matrix[0].size() * matrix.size())
-			{
-				iter = false;
-			}
-		}
-
-		return diagonalTraverseResult;
+    vector<int> diagonalTraverseResult;
+    
+    int lineM = matrix.size();
+    int lineN = matrix[0].size();
+    int matAllEle = lineM * lineN;
+    
+    if(matAllEle > 0)
+    {
+      bool isIter = true;
+      int iterCount = 0;
+    
+      int diagonalLinerM = 0;
+      int diagonalLinerN = 0;
+      int countEle = 0;
+    
+      while(isIter)
+      {
+        int m = diagonalLinerM;
+        int n = diagonalLinerN;
+        vector<int> tmpVct;
+      
+        while(true)
+        {
+          cout << m << endl;
+          cout << n << endl;
+          tmpVct.push_back(matrix[m][n]);
+          countEle++;
+          m--;
+          n++;
+        
+          if((m < 0) || (n >= lineN))
+          {
+            break;
+          }
+        }
+      
+        if(iterCount % 2 != 0)
+        {
+          reverse(tmpVct.begin(), tmpVct.end());
+        }
+      
+        diagonalTraverseResult.insert(diagonalTraverseResult.end(), tmpVct.begin(), tmpVct.end());
+      
+        if(diagonalLinerM < lineM - 1)
+        {
+          diagonalLinerM++;
+        }
+        else
+        {
+          diagonalLinerN++;
+        }
+      
+        if(matAllEle == countEle)
+        {
+          isIter = false;
+        }
+      
+        iterCount++;
+      
+        cout << "---" << endl;
+      }
     }
+    
+		return diagonalTraverseResult;
+  }
 };
 
 template <size_t m, size_t n>
@@ -116,33 +107,43 @@ vector<vector<int>> mtx2VctArray(int (&mtx)[m][n])
 {
 	vector<vector<int>> vctArray;
 
-    for (int i = 0; i < m; ++i) 
+  for (int i = 0; i < m; ++i) 
 	{
 		vector<int> tmpVct;
-        for (int j = 0; j < n; ++j) 
+		
+    for (int j = 0; j < n; ++j) 
 		{
-			// for printing
-            // cout << mtx[i][j] << " ";
 			tmpVct.push_back(mtx[i][j]);
-        }
-		// for printing
-        // cout << endl;
-		vctArray.push_back(tmpVct);
     }
+		
+		vctArray.push_back(tmpVct);
+  }
 	
 	return vctArray;
 }
 
 int main() 
 {
-	const size_t rowsM = 3;
-	const size_t columnsN = 3;
+  const size_t rowsM = 1;
+	const size_t columnsN = 1;
 
-	int mtx[rowsM][columnsN] = {1,2,3,4,5,6,7,8,9};
+	int mtx[rowsM][columnsN] = {1};
 
 	Solution s;
+	
+	vector<vector<int>> vctArray = mtx2VctArray(mtx);
 
-	vector<int> r0 = s.findDiagonalOrder(mtx2VctArray(mtx));
+	vector<int> r0 = s.findDiagonalOrder(vctArray);
+	
+	cout << "___result___" << endl;
+	
+	for(int i = 0; i < r0.size(); i++)
+	{
+	  cout << r0[i] << endl;
+	}
+	
+	cout << "___------___" << endl;
+	cout << "___endrst___" << endl;
 
 	return 0;
 }
