32e801e027cfbe49b1d8bebefab70e240a31fb64
ddk <eornrsayclub@gmail.com>
2020-09-20 22:39:48 +0900

    C++ study LeetCode : edit source code (Diagonal Traverse)

--- 03_C_plusplus/086_Diagonal Traverse/01_Diagonal_Traverse.cpp
+++ 03_C_plusplus/086_Diagonal Traverse/01_Diagonal_Traverse.cpp
@@ -33,71 +33,70 @@ class Solution
 {
 public:
   vector<int> findDiagonalOrder(vector<vector<int>>& matrix) 
-	{
+  {
     vector<int> diagonalTraverseResult;
+
+	if(matrix.size() == 0) { return diagonalTraverseResult; }
     
     int lineM = matrix.size();
     int lineN = matrix[0].size();
     int matAllEle = lineM * lineN;
     
-    if(matAllEle > 0)
+    bool isIter = true;
+    int iterCount = 0;
+    
+    int diagonalLinerM = 0;
+    int diagonalLinerN = 0;
+    int countEle = 0;
+    
+    while(isIter)
     {
-      bool isIter = true;
-      int iterCount = 0;
+      int m = diagonalLinerM;
+      int n = diagonalLinerN;
+      vector<int> tmpVct;
     
-      int diagonalLinerM = 0;
-      int diagonalLinerN = 0;
-      int countEle = 0;
-    
-      while(isIter)
+      while(true)
       {
-        int m = diagonalLinerM;
-        int n = diagonalLinerN;
-        vector<int> tmpVct;
+        cout << m << endl;
+        cout << n << endl;
+        tmpVct.push_back(matrix[m][n]);
+        countEle++;
+        m--;
+        n++;
       
-        while(true)
+        if((m < 0) || (n >= lineN))
         {
-          cout << m << endl;
-          cout << n << endl;
-          tmpVct.push_back(matrix[m][n]);
-          countEle++;
-          m--;
-          n++;
-        
-          if((m < 0) || (n >= lineN))
-          {
-            break;
-          }
+          break;
         }
-      
-        if(iterCount % 2 != 0)
-        {
-          reverse(tmpVct.begin(), tmpVct.end());
-        }
-      
-        diagonalTraverseResult.insert(diagonalTraverseResult.end(), tmpVct.begin(), tmpVct.end());
-      
-        if(diagonalLinerM < lineM - 1)
-        {
-          diagonalLinerM++;
-        }
-        else
-        {
-          diagonalLinerN++;
-        }
-      
-        if(matAllEle == countEle)
-        {
-          isIter = false;
-        }
-      
-        iterCount++;
-      
-        cout << "---" << endl;
       }
-    }
     
-		return diagonalTraverseResult;
+      if(iterCount % 2 != 0)
+      {
+        reverse(tmpVct.begin(), tmpVct.end());
+      }
+    
+      diagonalTraverseResult.insert(diagonalTraverseResult.end(), tmpVct.begin(), tmpVct.end());
+    
+      if(diagonalLinerM < lineM - 1)
+      {
+        diagonalLinerM++;
+      }
+      else
+      {
+        diagonalLinerN++;
+      }
+    
+      if(matAllEle == countEle)
+      {
+        isIter = false;
+      }
+    
+      iterCount++;
+    
+      cout << "---" << endl;
+    }
+	
+	return diagonalTraverseResult;
   }
 };
 
@@ -105,45 +104,42 @@ template <size_t m, size_t n>
 
 vector<vector<int>> mtx2VctArray(int (&mtx)[m][n]) 
 {
-	vector<vector<int>> vctArray;
+  vector<vector<int>> vctArray;
 
   for (int i = 0; i < m; ++i) 
-	{
-		vector<int> tmpVct;
-		
+  {
+	vector<int> tmpVct;	
     for (int j = 0; j < n; ++j) 
-		{
-			tmpVct.push_back(mtx[i][j]);
+	{
+	  tmpVct.push_back(mtx[i][j]);
     }
-		
 		vctArray.push_back(tmpVct);
   }
-	
-	return vctArray;
+  return vctArray;
 }
 
 int main() 
 {
   const size_t rowsM = 1;
-	const size_t columnsN = 1;
+  const size_t columnsN = 1;
 
-	int mtx[rowsM][columnsN] = {1};
+  int mtx[rowsM][columnsN] = {1};
 
-	Solution s;
+  Solution s;
 	
-	vector<vector<int>> vctArray = mtx2VctArray(mtx);
+  vector<vector<int>> vctArray = mtx2VctArray(mtx);
 
-	vector<int> r0 = s.findDiagonalOrder(vctArray);
+  vector<int> r0 = s.findDiagonalOrder(vctArray);
 	
-	cout << "___result___" << endl;
+  cout << "___result___" << endl;
 	
-	for(int i = 0; i < r0.size(); i++)
-	{
-	  cout << r0[i] << endl;
-	}
+  for(int i = 0; i < r0.size(); i++)
+  {
+    cout << r0[i] << endl;
+  }
 	
-	cout << "___------___" << endl;
-	cout << "___endrst___" << endl;
+  cout << "___------___" << endl;
+  cout << "___endrst___" << endl;
 
-	return 0;
+  return 0;
 }
