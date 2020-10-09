9b6ce456d8184b3cb363dbc433f6f3e5b7ed0445
Daeguk Do <38026518+daegukdo@users.noreply.github.com>
2020-10-09 22:28:30 +0900

    Update 01_Rotate_Array.cpp
    C++ study
    LeetCode : edit source code (Rotate Array)

--- 03_C_plusplus/099_Rotate Array/01_Rotate_Array.cpp
+++ 03_C_plusplus/099_Rotate Array/01_Rotate_Array.cpp
@@ -1,15 +1,10 @@
 /* 
 @ LeetCode
-
 Rotate Array
-
 Given an array, rotate the array to the right by k steps, where k is non-negative.
-
 Follow up:
-
 Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
 Could you do it in-place with O(1) extra space? 
-
 Example 1:
 Input: nums = [1,2,3,4,5,6,7], k = 3
 Output: [5,6,7,1,2,3,4]
@@ -17,7 +12,6 @@ Explanation:
 rotate 1 steps to the right: [7,1,2,3,4,5,6]
 rotate 2 steps to the right: [6,7,1,2,3,4,5]
 rotate 3 steps to the right: [5,6,7,1,2,3,4]
-
 Example 2:
 Input: nums = [-1,-100,3,99], k = 2
 Output: [3,99,-1,-100]
@@ -29,37 +23,52 @@ Constraints:
 1 <= nums.length <= 2 * 10^4
 It's guaranteed that nums[i] fits in a 32 bit-signed integer.
 k >= 0
-
 ref : https://leetcode.com/explore/learn/card/array-and-string/204/conclusion/1182/
 */
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-		// nums와 같은 사이즈(ns)의 vector를 하나 생성 = tmpVct
-		// 0부터 뒤에서 k칸만큼 이동한 위치(ns-k-1)까지를
-		// tmpVct에 k위치부터 붙여넣기
-		// 이후 0 ~ k-1까지는 남은 값을 작성
-
-        return;
+        int numsSize = nums.size();
+        if(numsSize <= k){
+            do
+                k -= numsSize;
+            while( numsSize <= k );
+        }
+        if(numsSize == 0 || k == 0) { return; }
+        
+        vector<int> tmpVct; //(numsSize + 1, 0);
+        
+        for(int i = numsSize - k; i < numsSize; i++){
+            tmpVct.push_back(nums[i]);
+        }
+        
+        for(int i = 0; i < numsSize - k; i++){
+            tmpVct.push_back(nums[i]);
+        }
+        
+        copy(tmpVct.begin(), tmpVct.end(), nums.begin());
     }
 };
 
 int main() {
-	int numsArr[7] = {1,2,3,4,5,6,7};
-	vector<int> nums(numsArr, *(&numsArr + 1));
+    int numsArr[7] = {1,2,3,4,5,6,7};
+    vector<int> nums(numsArr, *(&numsArr + 1));
 
-	int k = 3;
+    int k = 9;
 
-	Solution sol;
+    Solution sol;
 
-	sol.rotate(nums, k);
+    sol.rotate(nums, k);
 
-	return 0;
+    for(int i = 0; i < nums.size(); i++){
+        cout << nums[i] << endl;
+    }
+
+    return 0;
 }
