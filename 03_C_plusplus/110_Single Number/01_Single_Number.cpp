611d009dce2d3968501abc3727f235e66a8391bf
Daeguk Do <38026518+daegukdo@users.noreply.github.com>
2020-10-21 22:12:59 +0900

    Create 01_Single_Number.cpp
    C++ study
    LeetCode : add source code (Single Number)

---
+++ 03_C_plusplus/110_Single Number/01_Single_Number.cpp
@@ -0,0 +1,73 @@
+/* 
+@ LeetCode
+
+Single Number
+
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+Follow up: Could you implement a solution with a linear runtime complexity and without using extra memory?
+
+Example 1:
+Input: nums = [2,2,1]
+Output: 1
+
+Example 2:
+Input: nums = [4,1,2,1,2]
+Output: 4
+
+Example 3:
+Input: nums = [1]
+Output: 1
+ 
+Constraints:
+1 <= nums.length <= 3 * 104
+-3 * 104 <= nums[i] <= 3 * 104
+Each element in the array appears twice except for one element which appears only once.
+
+ref : https://leetcode.com/explore/learn/card/hash-table/183/combination-with-other-algorithms/1176/
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        vector<int> vctKey;
+        vector<int> vctNumOfKey;
+		int count = 0;
+
+		for(int i = 0; i < nums.size(); i++){
+		    auto it = find(vctKey.begin(), vctKey.end(), nums[i]);
+			if(it == vctKey.end()){
+				vctKey.push_back(nums[i]);
+				vctNumOfKey.push_back(1);
+			}
+			else{
+				vctNumOfKey[*it] += 1;
+			}
+		}
+
+		for(int i = 0; i < vctNumOfKey.size(); i++){
+			if(vctNumOfKey[i] == 1){
+				return vctKey[i];
+			}
+		}
+    }
+};
+
+int main() {
+	int numsArr[5] = {4,1,2,1,2};
+    vector<int> nums(begin(numsArr), end(numsArr));
+	
+	Solution sol;
+
+	int rst = sol.singleNumber(nums);
+
+	cout << "dup!" << endl;
+
+	return 0;
+}
