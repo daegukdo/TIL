cbee6ebafacca233430a9e5dbee6f3338624e13d
Daeguk Do <38026518+daegukdo@users.noreply.github.com>
2020-10-26 19:09:00 +0900

    Update 01_Happy_Number.cpp
    C++ study
    LeetCode : edit source code (Happy Number)

--- 03_C_plusplus/112_Happy Number/01_Happy_Number.cpp
+++ 03_C_plusplus/112_Happy Number/01_Happy_Number.cpp
@@ -1,35 +1,28 @@
 /* 
 @ LeetCode
-
 Happy Number
-
 Write an algorithm to determine if a number n is "happy".
-
 A happy number is a number defined by the following process:
 Starting with any positive integer, replace the number by the sum of the squares of its digits,
 and repeat the process until the number equals 1 (where it will stay),
 or it loops endlessly in a cycle which does not include 1. 
-
 Those numbers for which this process ends in 1 are happy numbers.
-
 Return True if n is a happy number, and False if not.
-
 Example: 
 Input: 19
 Output: true
-
 Explanation: 
 12 + 92 = 82
 82 + 22 = 68
 62 + 82 = 100
 12 + 02 + 02 = 1
-
 ref : https://leetcode.com/explore/learn/card/hash-table/183/combination-with-other-algorithms/1131/
 */
 
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_set>
 
 using namespace std;
 
@@ -41,6 +34,7 @@ public:
         int sum = 0;
         int tmpInt = 0;
         string tmpStr = "";
+		unordered_set<int> hashset;
         
         while(true){
             tmpStr = to_string(startNum);
@@ -55,8 +49,9 @@ public:
             
             if(sum == 1){
                 return true;
-            }else if(sum == n){
-                return false;
+            }else{
+				if(hashset.count(sum) > 0) { return false; }
+				else { hashset.insert(sum); }
             }
             
             startNum = sum - 0;
@@ -69,7 +64,7 @@ public:
 };
 
 int main() {
-    int n = 19;
+    int n = 2;
 	
 	Solution sol;
 
