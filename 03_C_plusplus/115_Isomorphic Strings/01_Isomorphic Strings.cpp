26e962e7e3b28423e16e6e69e6a115083da068e4
Daeguk Do <38026518+daegukdo@users.noreply.github.com>
2020-11-02 19:54:34 +0900

    Update 01_Isomorphic Strings.cpp
    C++ study
    LeetCode : edit source code (Isomorphic Strings)

--- 03_C_plusplus/115_Isomorphic Strings/01_Isomorphic Strings.cpp
+++ 03_C_plusplus/115_Isomorphic Strings/01_Isomorphic Strings.cpp
@@ -1,30 +1,21 @@
 /* 
 @ LeetCode
-
 Isomorphic Strings
-
 Given two strings s and t, determine if they are isomorphic.
-
 Two strings are isomorphic if the characters in s can be replaced to get t.
-
 All occurrences of a character must be replaced with another character while preserving the order of characters.
 No two characters may map to the same character but a character may map to itself.
-
 Example 1:
 Input: s = "egg", t = "add"
 Output: true
-
 Example 2:
 Input: s = "foo", t = "bar"
 Output: false
-
 Example 3:
 Input: s = "paper", t = "title"
 Output: true
-
 Note:
 You may assume both s and t have the same length.
-
 ref : https://leetcode.com/explore/learn/card/hash-table/184/comparison-with-other-data-structures/1117/
 */
 
@@ -35,40 +26,41 @@ ref : https://leetcode.com/explore/learn/card/hash-table/184/comparison-with-oth
 using namespace std;
 
 class Solution {
-private:
-	void setUnordered_mapData(string str, unordered_map<char, int> &hashmap){
-		for(int i = 0; i < str.length(); i++){
-			char tmpChar = str[i];
-			if(hashmap.count(tmpChar) == 0){
-				hashmap.insert(make_pair(tmpChar, 1));
-			}
-			else{
-				auto search = hashmap.find(tmpChar);
-				search->second += 1;
-			}
-		}
-	}
 public:
     bool isIsomorphic(string s, string t) {
-		unordered_map<char, int> hashmapS;
-		unordered_map<char, int> hashmapT;
+		unordered_map<char, char> hashmapST;
+		unordered_map<char, char> hashmapTS;
 
-		setUnordered_mapData(s, hashmapS);
-		setUnordered_mapData(t, hashmapT);
-
-		for(int i = 0; i < hashmapS.size(); i++){
-			if(hashmapS[s[i]] != hashmapT[t[i]]){
-				return false;
+		string compStr = "";
+		
+		for(int i = 0; i < s.length(); i++){
+			char tmpCharS = s[i];
+			char tmpCharT = t[i];
+			if(hashmapST.count(tmpCharS) == 0){
+				hashmapST.insert(make_pair(tmpCharS, tmpCharT));
+				compStr += tmpCharT;
 			}
+			else{
+				auto search = hashmapST.find(tmpCharS);
+				compStr += search->second;
+			}
+
+			if(hashmapTS.count(tmpCharT) == 0){
+				hashmapTS.insert(make_pair(tmpCharT, tmpCharS));
+			}
+
+			if(hashmapST.size() != hashmapTS.size()){ return false; }
 		}
 
-        return true;
+		if(compStr != t){ return false; }
+
+		return true;
     }
 };
 
 int main() {
-	string s = "ab";
-	string t = "aa";
+	string s = "aba";
+	string t = "baa";
 	
 	Solution sol;
 
