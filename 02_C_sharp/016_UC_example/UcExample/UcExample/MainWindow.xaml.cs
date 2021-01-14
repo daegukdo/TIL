40fb4b23e6d3eebdd95b1c5ea2a1d117548a69bb
ddk <eornrsayclub@gmail.com>
2021-01-14 21:17:19 +0900

    c# study add user control example

---
+++ 02_C_sharp/016_UC_example/UcExample/UcExample/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UcExample
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_click(object sender, RoutedEventArgs e)
+        {
+            txtBtn1.BtnTextStr = "btn1, font size 20";
+        }
+
+        private void btn2_click(object sender, RoutedEventArgs e)
+        {
+            txtBtn2.BtnTextStr = "btn2, font size 30";
+        }
+    }
+}
---
+++ 02_C_sharp/016_UC_example/UcExample/UcExample/UserControls/TextBtn.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UcExample.UserControls
+{
+    /// <summary>
+    /// TextBtn.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class TextBtn : UserControl
+    {
+        public TextBtn()
+        {
+            InitializeComponent();
+        }
+
+        public string BtnTextStr
+        {
+            get { return (string)GetValue(BtnTextStrProperty); }
+            set { SetValue(BtnTextStrProperty, value); }
+        }
+        public static readonly DependencyProperty BtnTextStrProperty =
+            DependencyProperty.Register("BtnTextStr", typeof(string), typeof(TextBtn), new UIPropertyMetadata("default"));
+
+        public event RoutedEventHandler Click;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Click(this, e);
+        }
+    }
+}
