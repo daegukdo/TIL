a3689931e8f4aeb6f88f73d42e0574f7bf0d13ee
ddk <eornrsayclub@gmail.com>
2021-02-13 21:44:16 +0900

    C++ study LeetCode : add issue description (Linked List Cycle II)

---
+++ 03_C_plusplus/131_Linked List Cycle II/01_Linked List Cycle II.cpp
@@ -0,0 +1,77 @@
+/* 
+@ LeetCode
+Linked List Cycle II
+
+Given a linked list, return the node where the cycle begins. 
+
+If there is no cycle, return null.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. 
+
+Internally, pos is used to denote the index of the node that tail's next pointer is connected to. 
+
+Note that pos is not passed as a parameter.
+
+Notice that you should not modify the linked list.
+
+Example 1:
+Input: head = [3,2,0,-4], pos = 1
+Output: tail connects to node index 1
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+Example 2:
+Input: head = [1,2], pos = 0
+Output: tail connects to node index 0
+Explanation: There is a cycle in the linked list, where tail connects to the first node.
+
+Example 3:
+Input: head = [1], pos = -1
+Output: no cycle
+Explanation: There is no cycle in the linked list.
+ 
+Constraints:
+The number of the nodes in the list is in the range [0, 104].
+-105 <= Node.val <= 105
+pos is -1 or a valid index in the linked-list.
+ 
+Follow up: Can you solve it using O(1) (i.e. constant) memory?
+
+ref : https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1214/
+*/
+
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        // head가 null인지 확인
+		// first와 second를 생성
+		// while로 반복해 가며 비교
+		// 0 1 2 3 1 2 3 1 2 3 ?
+
+    }
+};
+
+int main() {
+	ListNode* head = new ListNode(3);
+	head->next = new ListNode(2);
+	head->next->next = new ListNode(0);
+	head->next->next->next = new ListNode(-4);
+	head->next->next->next->next = head->next;
+
+	ListNode* _head = new ListNode(1);
+	
+	Solution sol;
+
+	bool rst = sol.detectCycle(_head);
+
+	return 0;
+}
