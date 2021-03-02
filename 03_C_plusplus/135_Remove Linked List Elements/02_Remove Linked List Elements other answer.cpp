59b19d7a81f0e330f5963485f772ac34a272a0bc
ddk <eornrsayclub@gmail.com>
2021-03-02 22:53:06 +0900

    C++ study LeetCode : edit other solution code (Remove Linked List Elements)

--- 03_C_plusplus/135_Remove Linked List Elements/02_Remove Linked List Elements other answer.cpp
+++ 03_C_plusplus/135_Remove Linked List Elements/02_Remove Linked List Elements other answer.cpp
@@ -9,6 +9,7 @@ Input:  1->2->6->3->4->5->6, val = 6
 Output: 1->2->3->4->5
 
 ref : https://leetcode.com/explore/learn/card/linked-list/219/classic-problems/1207/
+ref : https://medium.com/@jimdaosui/remove-linked-list-elements-6ec6b7560327
 */
 
 #include <iostream>
@@ -26,65 +27,37 @@ struct ListNode {
 
 class Solution {
 public:
-    ListNode* removeElements(ListNode* head, int val) {
-		if(head == NULL){
-			return head;
-		}
+	ListNode* removeElements(ListNode* head, int val) {
+		ListNode dummy(0);
+		ListNode* it = &dummy;
 
-		ListNode* pre_Ln = NULL;
-		ListNode* trg_Ln = head;
-		ListNode* postLn = NULL;
-
-		if(head->next != NULL){
-			postLn = head->next;
-		}
-
-		while(true){
-			if(trg_Ln->val == val){
-				if(pre_Ln != NULL){
-					// no first
-					trg_Ln = NULL;
-					pre_Ln->next = postLn;
-					if(pre_Ln->next != NULL){ trg_Ln = pre_Ln->next; }
-					else{ break; }
-					if(trg_Ln->next != NULL){ postLn = trg_Ln->next; }
-					else{ break; }
-				}
-				else{
-					// do first
-					head = postLn;
-					trg_Ln = head;
-					if(head->next != NULL){ postLn = head->next; }
-					else{ break; }
-				}
-			}
-			else{
-				pre_Ln = trg_Ln;
-				if(pre_Ln->next != NULL){ trg_Ln = pre_Ln->next; }
-				else{ break; }
-				if(trg_Ln->next != NULL){ postLn = trg_Ln->next; }
-				else{ postLn = NULL; }
+		while (head) {
+			if (head->val == val) {
+				ListNode* toDel = head;
+				head = head->next;
+				delete toDel;
+			} else {
+				it->next = head;
+				head = head->next;
+				it = it->next;
 			}
 		}
-
-		return head;
-    }
+		it->next = NULL;
+    
+		return dummy.next;
+	}
 };
 
 int main() {
-	// 1  2  3 4 5
-	// 2 1  3  4 5
-	// 3 2 1  4  5
-	// 4 3 2 1  5
 	ListNode* ln = new ListNode(1);
 	ln->next = new ListNode(2);
 	ln->next->next = new ListNode(3);
-	ln->next->next->next = new ListNode(4);
+	ln->next->next->next = new ListNode(2);
 	ln->next->next->next->next = new ListNode(5);
 
 	Solution sol;
 
-	ListNode* lnRst = sol.removeElements(ln, 5);
+	ListNode* lnRst = sol.removeElements(ln, 2);
 
 	return 0;
 }
