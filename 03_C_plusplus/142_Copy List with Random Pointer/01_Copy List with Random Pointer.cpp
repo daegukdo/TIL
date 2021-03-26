6f2c80cd94ae97a2225224d6f239228e7ef26b31
ddk <eornrsayclub@gmail.com>
2021-03-26 20:27:11 +0900

    C++ study LeetCode : add my test code (Copy List with Random Pointer)

--- 03_C_plusplus/142_Copy List with Random Pointer/01_Copy List with Random Pointer.cpp
+++ 03_C_plusplus/142_Copy List with Random Pointer/01_Copy List with Random Pointer.cpp
@@ -6,16 +6,19 @@ A linked list of length n is given such that each node contains an additional ra
 
 Construct a deep copy of the list. 
 The deep copy should consist of exactly n brand new nodes, where each new node has its value set to the value of its corresponding original node.
-Both the next and random pointer of the new nodes should point to new nodes in the copied list such that the pointers in the original list and copied list represent the same list state. 
+Both the next and random pointer of the new nodes should point to new nodes in the copied list 
+ such that the pointers in the original list and copied list represent the same list state. 
 None of the pointers in the new list should point to nodes in the original list.
 
-For example, if there are two nodes X and Y in the original list, where X.random --> Y, then for the corresponding two nodes x and y in the copied list, x.random --> y.
+For example, if there are two nodes X and Y in the original list, where X.random --> Y,
+ then for the corresponding two nodes x and y in the copied list, x.random --> y.
 
 Return the head of the copied linked list.
 
-The linked list is represented in the input/output as a list of n nodes. Each node is represented as a pair of [val, random_index] where:
-val: an integer representing Node.val
-random_index: the index of the node (range from 0 to n-1) that the random pointer points to, or null if it does not point to any node.
+The linked list is represented in the input/output as a list of n nodes. 
+Each node is represented as a pair of [val, random_index] where:
+ val: an integer representing Node.val
+ random_index: the index of the node (range from 0 to n-1) that the random pointer points to, or null if it does not point to any node.
 
 Your code will only be given the head of the original linked list.
 
@@ -42,20 +45,20 @@ Constraints:
 -10000 <= Node.val <= 10000
 Node.random is null or is pointing to some node in the linked list.
 
-Hide Hint #1  
+Hint #1  
 Just iterate the linked list and create copies of the nodes on the go. 
 Since a node can be referenced from multiple nodes due to the random pointers, make sure you are not making multiple copies of the same node.
 
-Hide Hint #2  
+Hint #2  
 You may want to use extra space to keep old node ---> new node mapping to prevent creating multiples copies of same node.
 
-Hide Hint #3  
+Hint #3  
 We can avoid using extra space for old node ---> new node mapping, by tweaking the original linked list. 
 Simply interweave the nodes of the old and copied list. For e.g.
 Old List: A --> B --> C --> D
 InterWeaved List: A --> A' --> B --> B' --> C --> C' --> D --> D'
 
-Hide Hint #4  
+Hint #4  
 The interweaving is done using next pointers and we can make use of interweaved structure to get the correct reference nodes for random pointers.
 
 ref : https://leetcode.com/explore/learn/card/linked-list/213/conclusion/1229/
@@ -82,15 +85,20 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        Node* rst = NULL;
+		Node rst = Node(0);
+        Node* rstIter = &rst;
 		Node* iter = head;
 
 		// deep copy?
 		while(iter){
+			rstIter = new Node(iter->val);
+			rstIter = rstIter->next;
+			iter = iter->next;
 
+			// how do you check iter's random 'Node'?
 		}
 
-		return rst;
+		return rst.next;
     }
 };
 
