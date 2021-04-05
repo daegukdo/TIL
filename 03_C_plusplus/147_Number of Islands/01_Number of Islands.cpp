14b4ab22a6ee2b5932012d609f220db10021e95d
ddk <eornrsayclub@gmail.com>
2021-04-05 22:44:06 +0900

    C++ study LeetCode : add issue description (Number of Islands)

---
+++ 03_C_plusplus/147_Number of Islands/01_Number of Islands.cpp
@@ -0,0 +1,57 @@
+/*
+@ LeetCode
+
+Number of Islands
+
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. 
+
+You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+
+Example 2:
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+ 
+Constraints:
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 300
+grid[i][j] is '0' or '1'.
+
+ref : https://leetcode.com/explore/learn/card/queue-stack/231/practical-application-queue/1374/
+*/
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+		const char land = '1';
+		const char water = '0';
+
+        return 0;
+    }
+};
+
+int main() {
+
+	return 0;
+}
