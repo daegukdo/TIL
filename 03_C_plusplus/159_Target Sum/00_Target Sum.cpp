c6f38a2af666efdf616ceab8a4049e337ca1d6ef
DaeGuk Do <eornrsayclub@gmail.com>
2021-06-08 20:46:42 +0900

    C++ study LeetCode : edit my test code (Target Sum)

--- 03_C_plusplus/159_Target Sum/00_Target Sum.cpp
+++ 03_C_plusplus/159_Target Sum/00_Target Sum.cpp
@@ -22,7 +22,7 @@ Explanation: There are 5 ways to assign symbols to make the sum of nums be targe
 Example 2:
 Input: nums = [1], target = 1
 Output: 1
- 
+
 Constraints:
 1 <= nums.length <= 20
 0 <= nums[i] <= 1000
@@ -35,42 +35,71 @@ ref : https://leetcode.com/explore/learn/card/queue-stack/232/practical-applicat
 #include <iostream>  
 #include <vector>
 #include <set>
+#include <math.h>
+#include <numeric>
 
 using namespace std;
 class Solution {
 private:
 	set<vector<char>> visitedOperators;
 
-	int summationData(char operatorChar, int ref, int trg){
+	int setSignNum(char operatorChar, int ref) {
 		int rst = 0;
 
-		if(operatorChar == '+')
-			rst = ref + trg;
-		else if(operatorChar == '-')
-			rst = ref - trg;
+		if (operatorChar == '+')
+			rst = ref;
+		else if (operatorChar == '-')
+			rst = -1 * ref;
 
 		return rst;
 	}
 public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-        // nums의 size만큼 vector<char> 생성 ex) + + +
+	int findTargetSumWays(vector<int>& nums, int target) {
+		return _findTargetSumWays(nums, target);
+	}
+
+	int _findTargetSumWays(vector<int>& nums, int target) {
+		// nums의 size만큼 vector<char> 생성 ex) + + +
 		// set<vector<char>> 생성해서 방문여부를 확인
 		// 없다면 추가하고 연산해서 target이라면 결과값에 플러스
 		// 아니라면 vector<char>의 값을 바꿈
 		int rst = 0;
 
+		if (nums.size() == 0) return rst;
+
+		if (nums.size() == 1) {
+			if (nums[0] == target) {
+				return 1;
+			}
+			else {
+				return 0;
+			}
+		}
+
 		vector<char> operators;
-		for(int i = 0; i < nums.size(); i++){
+		for (int i = 0; i < nums.size(); i++) {
 			operators.push_back('+');
 		}
 
-		//.... time and resource?
+		for(int i = 0; i < nums.size(); i++) {
+			vector<int> numsTmp;
+			for (int j = 0; j < nums.size(); j++) {
+				if (i != j) numsTmp.push_back(j);
+			}
+			rst += _findTargetSumWays(numsTmp, (target + setSignNum(operators[i], nums[i])));
+			operators[i] = '-';
+		}
 
 		return rst;
-    }
+	}
 };
 
 int main() {
+	vector<int> nums{ 1,1,1,1,1 };
+	int target = 3;
 
-    return 0;
+	Solution sol;
+	int rst = sol.findTargetSumWays(nums, target);
+
+	return 0;
 }
